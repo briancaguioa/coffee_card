011c9c89642fd8e205b102fcc227e66d1df39585
unknown <brian.gcaguioa@gmail.com>
2024-05-24 00:59:29 +0800

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:coffe_card/coffee_prefs.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Coffee ID',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text('How id like my coffee...'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const CoffeePrefs(),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:coffe_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MaterialApp(
+    home: Home()
+  ));
+}
+
